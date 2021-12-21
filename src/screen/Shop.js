40e9fd459e20dfe0a7e73a64334b9bdf76797c57
Mickaseg <mickael_seguin@hotmail.fr>
2021-12-21 17:59:12 +0100

    created filter

--- src/screen/Shop.js
+++ src/screen/Shop.js
@@ -21,47 +21,28 @@ const Characters = () => {
 
 	return (
         <div>
-		<div>
-			Characters !
-			<div className="GaleryCharacters">
-				{isLoading ? (
-					characters.map((character) => (
-						<Card
-							key={character.id}
-							id={character.id}
-							name={character.name}
-							image={character.image}
-							
-						/>
-					))
-				) : (
-					<div>...Loading</div>
-				)}
+			<div>
+				Characters !
+				<div className="GaleryCharacters">
+					{isLoading ? (
+						characters.map((character) => (
+							<Card
+								key={character.id}
+								id={character.id}
+								name={character.name}
+								image={character.image}
+								
+							/>
+						))
+					) : (
+						<div>...Loading</div>
+					)}
+				</div>
 			</div>
-		</div>
-	
+		
 
-            {/* <h1> Make your Choice </h1>
-            <select name="gender">
-                {characters.gender.map((element) => element.forEach(
-                    <option value={element}>{element}</option>))}
-
-            </select>
-            <select name="homeworld">
-                {characters.homeworld.map(element => element.forEach(
-                    <option value={element}>{element}</option>
-                ))}
-            </select>
-            <select name="haircolor">
-                {characters.haircolor.map((element)=> element.forEach(
-                    <option value={element}>{element}</option>
-                ))}
-            </select>
-            <select name="eyecolor">
-                {characters.eyecolor.map((element)=> element.forEach(
-                    <option value={element}>{element}</option>
-                ))}
-            </select> */}
+				<h1> Make your Choice </h1>
+				
         </div>
     )
 }
