57109ee8db220d90ffe9456e4326d8f6f823925f
Mickaseg <mickael_seguin@hotmail.fr>
2021-12-21 18:46:18 +0100

    added haircolor

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,78 +1,81 @@
 const Filter = () => {
+    const handle
     return(
         <div>
-			<h1> Make your Choice </h1>
+<h1> Make your Choice </h1>
     
 				<select name="gender">
-					<option>Male</option>
-					<option>Female</option>
-					<option>Droid</option>
+					<option value='male'>Male</option>
+					<option value='female'>Female</option>
+					<option value='droidgit stazt'>Droid</option>
 				</select>
+				
 
 				<select name="homeworld">
-					<option value='tatooine'>Tatooine</option>
-					<option value='naboo'>Naboo</option>
-					<option value='alderaan'>Alderaan</option>
-					<option value='stewjon'>Stewjon</option>
-					<option value='eriadu'>Eriadu</option>
-					<option value='kashyyyk'>Kashyyyk</option>
-					<option value='corellia'>Corellia</option>
-					<option value ='rodia'>Rodia</option>
-					<option value='bestine'>Bestine</option>
-					<option value='naboo'>Naboo</option>
-					<option value='kamino'>Kamino</option>
-					<option value='trandosha'>Trandosha</option>
-					<option value='socorro'>Socorro</option>
-					<option value='bespin'>Bespin</option>
-					<option value='cala'>Mon Cala</option>
-					<option value='chandrila'>Chandrila</option>
-					<option value='endor'>Endor</option>
-					<option value='sullust'>Sullust</option>
-					<option value='coruscant'>Coruscant</option>
-					<option value='neimoidia'>Neimoidia</option>
-					<option value='coruscant'>Coruscant</option>
-					<option value='toydaria'>Toydaria</option>
-					<option value='malastare'>Malastare</option>
-					<option value='dathomir'>Dathomir</option>
-					<option value='ryloth'>Ryloth</option>
-					<option value='aleen'>Aleen</option>
-					<option value='vulpter'>Vulpter</option>
-					<option value='troiken'>Troiken</option>
-					<option value='tund'>Tund</option>
-					<option value='haruun'>Haruun Kai</option>
-					<option value='cerea'>Cerea</option>
-					<option value='glee anselm'>Glee Anselm</option>
-					<option value='nar shaddaa'>Nar Shaddaa</option>
-					<option value='iktotch'>Iktotch</option>
-					<option value='quermia'>Quermia</option>
-					<option value='dorin'>Dorin</option>
-					<option value='champala'>Champala</option>
-					<option value='geonosis'>Geonosis</option>
-					<option value='mirial'>Mirial</option>
-					<option value='serenno'>Serenno</option>
-					<option value='concord dawn'>Concord Dawn</option>
-					<option value='zolan'>Zolan</option>
-					<option value='ojom'>Ojom</option>
-					<option value='skako'>Skako</option>
-					<option value='scipio'>Scipio</option>
-					<option value='shili'>Shili</option>
-					<option value='kalee'>Kalee</option>
-					<option value='utapau'>Utapau</option>
-					<option value='yavin 4'>Yavin 4</option>
-					<option value='jakku'>Jakku</option>
-					<option value='parnassos'>Parnassos</option>
+				<option value='tatooine'>Tatooine</option>
+				<option value='naboo'>Naboo</option>
+				<option value='alderaan'>Alderaan</option>
+				<option value='stewjon'>Stewjon</option>
+				<option value='eriadu'>Eriadu</option>
+				<option value='kashyyyk'>Kashyyyk</option>
+				<option value='corellia'>Corellia</option>
+				<option value ='rodia'>Rodia</option>
+				<option value='bestine'>Bestine</option>
+				<option value='naboo'>Naboo</option>
+				<option value='kamino'>Kamino</option>
+				<option value='trandosha'>Trandosha</option>
+				<option value='socorro'>Socorro</option>
+				<option value='bespin'>Bespin</option>
+				<option value='cala'>Mon Cala</option>
+				<option value='chandrila'>Chandrila</option>
+				<option value='endor'>Endor</option>
+				<option value='sullust'>Sullust</option>
+				<option value='coruscant'>Coruscant</option>
+				<option value='neimoidia'>Neimoidia</option>
+				<option value='coruscant'>Coruscant</option>
+				<option value='toydaria'>Toydaria</option>
+				<option value='malastare'>Malastare</option>
+				<option value='dathomir'>Dathomir</option>
+				<option value='ryloth'>Ryloth</option>
+                <option value='aleen'>Aleen</option>
+                <option value='vulpter'>Vulpter</option>
+                <option value='troiken'>Troiken</option>
+                <option value='tund'>Tund</option>
+                <option value='haruun'>Haruun Kai</option>
+                <option value='cerea'>Cerea</option>
+                <option value='glee anselm'>Glee Anselm</option>
+                <option value='nar shaddaa'>Nar Shaddaa</option>
+                <option value='iktotch'>Iktotch</option>
+                <option value='quermia'>Quermia</option>
+                <option value='dorin'>Dorin</option>
+                <option value='champala'>Champala</option>
+                <option value='geonosis'>Geonosis</option>
+                <option value='mirial'>Mirial</option>
+                <option value='serenno'>Serenno</option>
+                <option value='concord dawn'>Concord Dawn</option>
+                <option value='zolan'>Zolan</option>
+                <option value='ojom'>Ojom</option>
+                <option value='skako'>Skako</option>
+                <option value='scipio'>Scipio</option>
+                <option value='shili'>Shili</option>
+                <option value='kalee'>Kalee</option>
+                <option value='utapau'>Utapau</option>
+                <option value='yavin 4'>Yavin 4</option>
+                <option value='jakku'>Jakku</option>
+                <option value='parnassos'>Parnassos</option>
 				</select>
 
 				<select name="haircolor">
-					<option value='blond'>Blond</option>
-					<option value='brown, later graying'>Brown later graying'</option>
-					<option value='brown, later gray'>Brown later gray</option>
-					<option value='brown'>Brown</option>
-					<option value='black'>Black</option>
-					<option value='light brown, later white'>Light brown, later white</option>
-					<option value='auburn, later gray'>Auburn, later gray</option>
-					<option value='white'>White</option>
-					<option value='auburn'>Auburn</option>
+					<option value=''></option>
+					<option value=''></option>
+					<option value=''></option>
+					<option value=''></option>
+					<option value=''></option>
+					<option value=''></option>
+					<option value=''></option>
+					<option value=''></option>
+					<option value=''></option>
+					<option value=''></option>
 				</select>
 				<select name="eyecolor">
 					<option value='blue'>Blue</option>
