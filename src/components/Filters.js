27e4b2f85b5bc6eb20815ffa233979f121ba9125
Thymus <thymus@pop-os.localdomain>
2021-12-21 18:39:50 +0100

    modif indent

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,67 +1,66 @@
 const Filter = () => {
     return(
         <div>
-<h1> Make your Choice </h1>
+			<h1> Make your Choice </h1>
     
 				<select name="gender">
 					<option>Male</option>
 					<option>Female</option>
 					<option>Droid</option>
 				</select>
-				
 
 				<select name="homeworld">
-				<option value='tatooine'>Tatooine</option>
-				<option value='naboo'>Naboo</option>
-				<option value='alderaan'>Alderaan</option>
-				<option value='stewjon'>Stewjon</option>
-				<option value='eriadu'>Eriadu</option>
-				<option value='kashyyyk'>Kashyyyk</option>
-				<option value='corellia'>Corellia</option>
-				<option value ='rodia'>Rodia</option>
-				<option value='bestine'>Bestine</option>
-				<option value='naboo'>Naboo</option>
-				<option value='kamino'>Kamino</option>
-				<option value='trandosha'>Trandosha</option>
-				<option value='socorro'>Socorro</option>
-				<option value='bespin'>Bespin</option>
-				<option value='cala'>Mon Cala</option>
-				<option value='chandrila'>Chandrila</option>
-				<option value='endor'>Endor</option>
-				<option value='sullust'>Sullust</option>
-				<option value='coruscant'>Coruscant</option>
-				<option value='neimoidia'>Neimoidia</option>
-				<option value='coruscant'>Coruscant</option>
-				<option value='toydaria'>Toydaria</option>
-				<option value='malastare'>Malastare</option>
-				<option value='dathomir'>Dathomir</option>
-				<option value='ryloth'>Ryloth</option>
-                <option value='aleen'>Aleen</option>
-                <option value='vulpter'>Vulpter</option>
-                <option value='troiken'>Troiken</option>
-                <option value='tund'>Tund</option>
-                <option value='haruun'>Haruun Kai</option>
-                <option value='cerea'>Cerea</option>
-                <option value='glee anselm'>Glee Anselm</option>
-                <option value='nar shaddaa'>Nar Shaddaa</option>
-                <option value='iktotch'>Iktotch</option>
-                <option value='quermia'>Quermia</option>
-                <option value='dorin'>Dorin</option>
-                <option value='champala'>Champala</option>
-                <option value='geonosis'>Geonosis</option>
-                <option value='mirial'>Mirial</option>
-                <option value='serenno'>Serenno</option>
-                <option value='concord dawn'>Concord Dawn</option>
-                <option value='zolan'>Zolan</option>
-                <option value='ojom'>Ojom</option>
-                <option value='skako'>Skako</option>
-                <option value='scipio'>Scipio</option>
-                <option value='shili'>Shili</option>
-                <option value='kalee'>Kalee</option>
-                <option value='utapau'>Utapau</option>
-                <option value='yavin 4'>Yavin 4</option>
-                <option value='jakku'>Jakku</option>
-                <option value='parnassos'>Parnassos</option>
+					<option value='tatooine'>Tatooine</option>
+					<option value='naboo'>Naboo</option>
+					<option value='alderaan'>Alderaan</option>
+					<option value='stewjon'>Stewjon</option>
+					<option value='eriadu'>Eriadu</option>
+					<option value='kashyyyk'>Kashyyyk</option>
+					<option value='corellia'>Corellia</option>
+					<option value ='rodia'>Rodia</option>
+					<option value='bestine'>Bestine</option>
+					<option value='naboo'>Naboo</option>
+					<option value='kamino'>Kamino</option>
+					<option value='trandosha'>Trandosha</option>
+					<option value='socorro'>Socorro</option>
+					<option value='bespin'>Bespin</option>
+					<option value='cala'>Mon Cala</option>
+					<option value='chandrila'>Chandrila</option>
+					<option value='endor'>Endor</option>
+					<option value='sullust'>Sullust</option>
+					<option value='coruscant'>Coruscant</option>
+					<option value='neimoidia'>Neimoidia</option>
+					<option value='coruscant'>Coruscant</option>
+					<option value='toydaria'>Toydaria</option>
+					<option value='malastare'>Malastare</option>
+					<option value='dathomir'>Dathomir</option>
+					<option value='ryloth'>Ryloth</option>
+					<option value='aleen'>Aleen</option>
+					<option value='vulpter'>Vulpter</option>
+					<option value='troiken'>Troiken</option>
+					<option value='tund'>Tund</option>
+					<option value='haruun'>Haruun Kai</option>
+					<option value='cerea'>Cerea</option>
+					<option value='glee anselm'>Glee Anselm</option>
+					<option value='nar shaddaa'>Nar Shaddaa</option>
+					<option value='iktotch'>Iktotch</option>
+					<option value='quermia'>Quermia</option>
+					<option value='dorin'>Dorin</option>
+					<option value='champala'>Champala</option>
+					<option value='geonosis'>Geonosis</option>
+					<option value='mirial'>Mirial</option>
+					<option value='serenno'>Serenno</option>
+					<option value='concord dawn'>Concord Dawn</option>
+					<option value='zolan'>Zolan</option>
+					<option value='ojom'>Ojom</option>
+					<option value='skako'>Skako</option>
+					<option value='scipio'>Scipio</option>
+					<option value='shili'>Shili</option>
+					<option value='kalee'>Kalee</option>
+					<option value='utapau'>Utapau</option>
+					<option value='yavin 4'>Yavin 4</option>
+					<option value='jakku'>Jakku</option>
+					<option value='parnassos'>Parnassos</option>
 				</select>
 
 				<select name="haircolor">
