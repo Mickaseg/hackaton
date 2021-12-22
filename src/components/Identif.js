f3fd792ab0d765b466198d353ef1d082d9ec50c5
Marsaleix Romain <msx.coach@gmail.com>
2021-12-22 13:21:28 +0100

    dernier jet

--- src/components/Identif.js
+++ src/components/Identif.js
@@ -12,35 +12,35 @@ return <div className="main">
 
   <div className="login-box"> 
   <div><h2 id="registrationTitle">Registration Form</h2></div>
-  <form>
+<form>
     <div className="user-box">
-      <input type="text" name="lastname" required placeholder=""/>
-      <label>Last Name</label>
+      <input type="text" name="user_lastname" required="Hey Nasty, don't forget your lastname! " placeholder=""/>
+      <label for="lastname">Last Name</label>
     </div>
     <div className="user-box">
-      <input type="text" name="firstname" required placeholder="" />
-      <label>First Name</label>
+      <input type="text" name="user_firstname" required="Hey Cutie, what's your firstname?" placeholder="" />
+      <label for="firstname">First Name</label>
     </div>
     <div className="user-box">
-      <input type="text" name="telephone" required placeholder="" />
-      <label>Telephone</label>
+      <input type="text" name="user_telephone" required="Hey Honey, you forgot to write your number :)" placeholder="" />
+      <label for="telephone">Telephone</label>
     </div>
     <div className="user-box">
-      <input type="email" name="email" required placeholder="" />
+      <input type="email" name="user_email" required="Hey Honey, you forgot to write your email :)" placeholder="" />
     <label for="email">Email</label>
     </div>
-    <div className="user-box">
+    <div className="user-box"> 
       <input type="text" name="" required />
     <label>Give us the name of your favorite target !</label>
     </div>
-    <a href="">
+    <a href="#"  > 
       <span></span>
       <span></span>
       <span></span>
       <span></span>
       Submit
     </a>
-  </form>
+</form>
 </div>
 <img src={bande} alt="bande" className="form-bande" />
 
